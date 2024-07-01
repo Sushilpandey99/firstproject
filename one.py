a285b88118051f1327ab72bb134b2fdc29aa6617
meekhum <masteraanime818@gmail.com>
2024-07-01 11:55:04 +0545

    changing

--- one.py
+++ one.py
@@ -1,1 +1,11 @@
 print("hello ")
+print("changing")
+import math
+n=int(input('Enter the number of patten you want:\t'))
+i=n
+count=1
+while count<=n:
+    fun=math.log(count)
+    fun=int(fun)
+    print('*'*fun)
+    count=count+1
